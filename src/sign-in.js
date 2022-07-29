4152e4c2266ca80b9235ce730b376d63f16bd3e2
Wendell Liu <wendell.liu@houzz.com>
2022-07-29 17:01:30 +0800

    feat: sign in

---
+++ src/sign-in.js
@@ -0,0 +1,37 @@
+import { sleep, check } from 'k6';
+
+import createUserSignUp from './lib/generators/create-user-sign-up';
+import post from './lib/http/post';
+
+const endpoint = __ENV.ENDPOINT;
+
+export function setup() {
+  const url = `${endpoint}/api/users`;
+  const userData = createUserSignUp();
+  post(url, userData);
+
+  return userData;
+}
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<200'],
+  },
+};
+
+export default function (userData) {
+  const url = `${endpoint}/api/signin`;
+  const { email, password } = userData;
+
+  const res = post(url, {
+    email,
+    password,
+  });
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
